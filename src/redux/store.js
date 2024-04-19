47deb44f157383c26cb62eb9cdc1f0828d7157a2
amitark7 <kumaramit21470@gmail.com>
2024-04-19 19:28:12 +0530

    remove extra comment

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./reducer/todoReducer";
-import modalReducer from "./reducer/modalReducer";
 
 export const store = configureStore({
   reducer: {
     todos: todoReducer,
-    showModal: modalReducer,
   },
 });
